3a3fa6c2ee1e0a494380032a89cab4fbf234ad23
deonisiu <deonisiu@ya.ru>
2025-07-20 12:22:31 +0700

    update

--- router.js
+++ router.js
@@ -6,7 +6,7 @@ export async function router() {
   const path = window.location.pathname;
 
   if (path === '/' || path === '/index.html') {
-    app.innerHTML = '<p>Добро пожаловать в PWA! Выберите игру в меню.</p>';
+    app.innerHTML = '<p>Добро пожаловать в PWA v0.1! Выберите игру в меню.</p>';
   } else if (path === '/game1') {
     app.innerHTML = '<div id="unityContainer" style="width: 100%; height: 600px;"></div>';
     await loadUnityGame('unityContainer', '/unity-modules/game1');
